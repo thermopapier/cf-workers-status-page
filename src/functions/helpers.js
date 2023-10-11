f791466a7501cb0935b16bd66c523d49880e020e
cyber swiper <105233410+cyber-swiper@users.noreply.github.com>
2023-10-11 13:50:26 +0200

    Update helpers.js

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -94,9 +94,7 @@ export async function notifyDiscord(monitor, operational) {
         title: `${monitor.name} is ${getOperationalLabel(operational)} ${
           operational ? ':white_check_mark:' : ':x:'
         }`,
-        description: `\`${monitor.method ? monitor.method : 'GET'} ${
-          monitor.url
-        }\` - :eyes: [Status Page](${config.settings.url})`,
+        description: `More infos [here](${config.settings.url}).`,
         color: operational ? 3581519 : 13632027,
       },
     ],
